9992bb9e917b6b51fe7651b9b9dd9c2879f4114f
Rodrigo <contactorodrigocabrera@gmail.com>
2023-10-09 22:09:59 -0300

    trabajo sobre bucle while

--- guess_the_number/adivina_numero.py
+++ guess_the_number/adivina_numero.py
@@ -10,3 +10,8 @@ def adivina_el_numero(x):
     #randint es rand por random int por entero. Lo que devuelve un numero entero aleatorio. 
     #Recibe dos parametros: a y b. El numero que se genera, va a ser mayor o igual a a, y menor o igual a b.
     numero_aleatorio = random.randint(1, x)
+
+    prediccion = 0 #el valor inicial lo ponemos en 0 para que no coincida de ninguna manera con el numero random. Si le asignamos 1, por ejemplo, y el numero random es 1, el usuario no tiene rondas para jugar porque el número ya está adivinado.
+
+    while prediccion != numero_aleatorio:
+        prediccion = int(input(f"Adivina un número entre 1 y {x}: ")) #El valor que ingrese el usuario, por el input, se asignará a la variable prediccion. El int inicial, trabaja lo ingresado como un número, ya que input siempre toma lo ingresado como string.
