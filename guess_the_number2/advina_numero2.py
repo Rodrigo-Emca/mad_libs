1c143149f2c8f56b784734370204f8445ca5da9a
Rodrigo <contactorodrigocabrera@gmail.com>
2023-10-10 22:04:00 -0300

    Inicio del tercer proyecto

---
+++ guess_the_number2/advina_numero2.py
@@ -0,0 +1,7 @@
+
+
+def adivina_el_numero_computadora(x):
+    print("==================================")
+    print("    ¡Bienvenido/a al Juego!       ")
+    print("==================================")
+    print(f"Selecciona un número entre 1 y {x} para que la computadora intente adivinar.")
