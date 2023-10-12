29de4bf77050da0a63a359ab2b8da7ff2935d4c5
Rodrigo <contactorodrigocabrera@gmail.com>
2023-10-12 18:06:40 -0300

    Condicionales if y elif para medir las respuestas y reformular prediciones

--- guess_the_number2/advina_numero2.py
+++ guess_the_number2/advina_numero2.py
@@ -20,4 +20,14 @@ def adivina_el_numero_computadora(x):
             prediccion = limite_inferior
 
         #Obtener respuesta del usuario.
-        respuesta = input(f"Mi predicción es {prediccion}. Si es muy alta, ingresa (A). Si es muy baja, ingresa (B). Si es correcta, ingresa (C).")
+        #Usamos .lower para prevenir que el usuario ingrese una letra en minuscula.
+        respuesta = input(f"Mi predicción es {prediccion}. Si es muy alta, ingresa (A). Si es muy baja, ingresa (B). Si es correcta, ingresa (C).").lower
+
+        if respuesta == "a":
+            limite_superior = prediccion - 1
+            #Frente a la respuesta del usuario a, la computadora reformulara su predicción restandole 1.
+            # Si el usuario piensa en el numero 6, la computadora predijo el numero 8, y el usuario responde "a", no hace falta buscar entre todos los numeros nuevamente, sino que debe tomar su predicción (8), restarle 1 y volver a preguntar. 
+        elif respuesta == "b":
+            limite_inferior = prediccion + 1
+
+    print(f"¡Siii! La computadora adivinó tu número correctamente: {prediccion}")
