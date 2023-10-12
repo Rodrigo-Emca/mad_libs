e2270bea0fbf70c65b5995a3880c81896f0226de
Rodrigo <contactorodrigocabrera@gmail.com>
2023-10-12 17:53:37 -0300

    creacion de bucle while para iteracion de las predicciones

--- guess_the_number2/advina_numero2.py
+++ guess_the_number2/advina_numero2.py
@@ -1,7 +1,22 @@
-
+import random
 
 def adivina_el_numero_computadora(x):
     print("==================================")
     print("    ¡Bienvenido/a al Juego!       ")
     print("==================================")
-    print(f"Selecciona un número entre 1 y {x} para que la computadora intente adivinar.")
+    print(f"Selecciona un número entre 1 y {x} parza que la computadora intente adivinar.")
+
+    limite_inferior = 1
+    limite_superior = x
+
+    respuesta = ""
+    
+    #Usamos un ciclo while porque no sabemos cuántos intentos va a requerir, cuantas itereaciones vamos a necesitar
+    while respuesta != "c":
+        #generar predicción
+        if limite_inferior != limite_superior:
+            prediccion = random.randint(limite_inferior, limite_superior)
+        else: 
+            prediccion = limite_inferior
+
+        #Obtener respuesta del usuario.
