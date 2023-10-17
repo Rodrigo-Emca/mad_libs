f8657f5152667dca74dde996827109a339bb39e7
Rodrigo <contactorodrigocabrera@gmail.com>
2023-10-17 00:10:11 -0300

    inicio hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,2 +1,22 @@
+import random
+
+#Las palabras las tenemos en un archivo separado. Importamos un elemento de un archivo, que nosotros queramos.
+from palabras import palabras
+
+def obtener_palabra_valida(palabras):
+    #Seleccionar una palabra al azar de la lista
+    palabra = random.choice(palabras)
+
+    while '-' in palabra or ' ' in  palabra:
+        palabra = random.choice(palabras)
+
+    return palabra.upper() 
 
 
+def ahorcado():
+
+    print("==============================================")
+    print("    ¡Bienvenido/a al Juego del Ahorcado!      ")
+    print("==============================================")
+
+    palabra = obtener_palabra_valida(palabras)
